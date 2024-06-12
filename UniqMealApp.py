c45e58cbf6c34ff00e12f0b685c55fceab5af72a
Al-Miraj <khadija.arkhouch@outlook.com>
2024-06-12 21:17:59 +0200

    Added a setup for the application in which you can run some code

--- UniqMealApp.py
+++ UniqMealApp.py
@@ -1,5 +1,32 @@
-from InputChecker import InputChecker
+# from InputChecker import InputChecker
+from Roles.User import User
+from Roles.Member import Member
+from Roles.Consultant import Consultant
+from Roles.SystemAdmin import SystemAdmin
+from Roles.SuperAdmin import SuperAdmin
+from Database import DBConfig, DBConnectionManager
+from Database.DataAccesObjects.UsersDAO import UsersDAO
+
+import sqlite3
+import os
+import sys
+
+
+def read_json_file(filename) -> list:  # buiten class?
+
+    with open(os.path.join(os.path.dirname(__file__), filename), encoding="utf-8") as file:
+
+        contents = json.load(file)
+    return contents
+
 
 if __name__ == "__main__":
-    inputnum = input("input your phone number:\n+31-6-")
-    print(InputChecker.checkPhoneNumber("+31-6-" + inputnum12))
+    dbFile = DBConfig.DBConfig.DBFile
+    DCMobj = DBConnectionManager.DCM(dbFile)
+    DCMobj.connect()
+    conn = DCMobj.conn
+    cursor = conn.cursor()
+    #enter your code here :)
+
+
+
