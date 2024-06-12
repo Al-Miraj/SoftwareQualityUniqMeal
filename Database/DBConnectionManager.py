d2ed65b0f157a5a66ec4254e7ed32b1272e54a5d
Al-Miraj <khadija.arkhouch@outlook.com>
2024-06-12 21:05:40 +0200

    created a json file with mock user data. used it for populating the users table with mock data in UniqueMealDB. Data in this file has not been checked for legitimacy.

--- Database/DBConnectionManager.py
+++ Database/DBConnectionManager.py
@@ -1,15 +1,20 @@
+import os
+import sys
 import sqlite3
+
 
 class DCM:
     """
     DCM stands for Database Connection Manager.
+
+    This file should be in the same directory are the database for self.connect to generate the correct path.
     """
-    def __init__(self, db_file):
-        self.db_file = db_file
+    def __init__(self, dbFile):
+        self.dbFile = dbFile
         self.conn = None
 
     def connect(self):
-        self.conn = sqlite3.connect(self.db_file)
+        self.conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), self.dbFile))
 
     def disconnect(self):
         if self.conn:
